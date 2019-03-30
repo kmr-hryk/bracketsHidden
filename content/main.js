a794d66181d320237e4d0ac5aeb33e9f4898e332
Hiroyuki Kimura <z_kimura@k-side.net>
2019-03-30 23:29:21 +0900

    ダミー括弧の表示を追加

--- content/main.js
+++ content/main.js
@@ -7,20 +7,25 @@ $(function() {
   $('#right_content').html(
     $('#right_content')
       .html()
-      .replace(/）/g, '）</span>')
+      .replace(/）/g, '）</span><span class="emptyBrackets">()</span>')
   );
+
+  $('.emptyBrackets').css('display', 'none');
 
   chrome.runtime.onMessage.addListener((btn, sender, res) => {
     if (btn === 'hideBtn') {
       $('.brackets').css('display', 'none');
       $('.brackets').css('width', '0px');
       $('.brackets').css('color', '');
+      $('.emptyBrackets').css('display', '');
     } else if (btn === 'grayBtn') {
       $('.brackets').css('display', '');
       $('.brackets').css('color', 'lightgray');
+      $('.emptyBrackets').css('display', 'none');
     } else {
       $('.brackets').css('display', '');
       $('.brackets').css('color', '');
+      $('.emptyBrackets').css('display', 'none');
     }
     res(btn);
   });
